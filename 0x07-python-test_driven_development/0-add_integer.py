2388aa6316e25839d0895918e2079fbe48889d59
Bboy010 <hkoffianderson@gmail.com>
2023-02-03 12:17:02 +0000

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-"""
-Module add-integer
-add two integers
-"""
+"""A function that adds two integers."""
 
 
 def add_integer(a, b=98):
-    """ This function return the sum of intgers
-    or the int cast of floats
-    """
-
-    if type(a) != int and type(a) != float:
+    """A function that return int(a) + int(b)."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-
-    return a + b
+    return int(a) + int(b)
