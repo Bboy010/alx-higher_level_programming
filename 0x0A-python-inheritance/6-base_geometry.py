48a744052fe15cb0949180c495acaea93a0a1638
Bboy010 <hkoffianderson@gmail.com>
2023-02-07 11:35:08 +0000

    Improve gometry code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" creating a class to improve geometry """
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        area method
+        """
+        raise Exception("area() is not implemented")
